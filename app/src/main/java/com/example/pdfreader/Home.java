776fecd572ccb2183838bff5136353b0ac94dbba
quannar178 <quannar178@gmail.com>
2022-04-02 13:24:57 +0700

    Done APP with splash

--- app/src/main/java/com/example/pdfreader/Home.java
+++ app/src/main/java/com/example/pdfreader/Home.java
@@ -1,45 +1,50 @@
 package com.example.pdfreader;
 
-import android.content.Intent;
-import android.os.Handler;
-import android.view.Window;
-import android.view.WindowManager;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.os.PersistableBundle;
+
 import com.github.barteksc.pdfviewer.PDFView;
+import com.github.barteksc.pdfviewer.listener.OnLoadCompleteListener;
+import com.github.barteksc.pdfviewer.listener.OnPageChangeListener;
 import com.github.barteksc.pdfviewer.scroll.DefaultScrollHandle;
 import com.github.barteksc.pdfviewer.util.FitPolicy;
 
-public class MainActivity extends AppCompatActivity {
+public class Home extends AppCompatActivity implements OnPageChangeListener, OnLoadCompleteListener {
 
+    private int pageNumber = 0;
+    private PDFView pdfView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE); //will hide the title
+
         getSupportActionBar().hide();
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_home);
 
-        final Runnable r = new Runnable() {
-            public void run() {
-                Intent intent = new Intent(getApplicationContext(), Home.class);
-                startActivity(intent);
-                finish();
-            }
-        };
+        pdfView = (PDFView) findViewById(R.id.pdfView);
 
-        Handler handler = new Handler();
+        pdfView.fromAsset("test.pdf")
+                .defaultPage(pageNumber)
+                .onPageChange(this)
+                .onLoad(this)
+                .enableAnnotationRendering(true)
+                .scrollHandle(new DefaultScrollHandle(this))
+                .spacing(10) // in dp
+                .pageFitPolicy(FitPolicy.WIDTH)
+//                .pageSnap(true)
+//                .pageFling(true)
+                .load();
 
-        handler.postDelayed(r, 3000);
-
-
-//        PDFView pdfView = (PDFView) findViewById(R.id.pdfView);
-//
 //        pdfView.fromAsset("test.pdf")
 ////                .pages(0, 2, 1, 3, 3, 3) // all pages are displayed by default
 //                .enableSwipe(true) // allows to block changing pages using swipe
 //                .swipeHorizontal(false)
 //                .enableDoubletap(true)
 //                .defaultPage(0)
+//                .enableAnnotationRendering(true)
+//                .scrollHandle(new DefaultScrollHandle(this))
 //                // allows to draw something on the current page, usually visible in the middle of the screen
 ////                .onDraw(onDrawListener)
 //                // allows to draw something on all pages, separately for every page. Called only for visible pages
@@ -59,14 +64,40 @@ public class MainActivity extends AppCompatActivity {
 //                .enableAntialiasing(true) // improve rendering a little bit on low-res screens
 //                // spacing between pages in dp. To define spacing color, set view background
 //                .spacing(0)
-//                .autoSpacing(false) // add dynamic spacing to fit each page on its own on the screen
+//                .autoSpacing(true) // add dynamic spacing to fit each page on its own on the screen
 ////                .linkHandler(DefaultLinkHandler)
-//                .pageFitPolicy(FitPolicy.WIDTH) // mode to fit pages in the view
+//                .pageFitPolicy(FitPolicy.BOTH) // mode to fit pages in the view
 //                .fitEachPage(false) // fit each page to the view, else smaller pages are scaled relative to largest page.
-//                .pageSnap(false) // snap pages to screen boundaries
+//                .pageSnap(true) // snap pages to screen boundaries
 //                .pageFling(false) // make a fling change only a single page like ViewPager
 //                .nightMode(false) // toggle night mode
 //                .load();
+    }
 
+    @Override
+    public void onPageChanged(int page, int pageCount) {
+        pageNumber = page;
+        System.out.println(String.format("%s / %s", pageNumber + 1, pageCount));
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        System.out.println("onSaveInstanceState" + pageNumber);
+        outState.putInt("currentPage", pageNumber);
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        System.out.println("onRestoreInstanceState" + savedInstanceState.getInt("currentPage"));
+        pageNumber = +savedInstanceState.getInt("currentPage");
+        System.out.println("onRestoreInstanceState" + pageNumber);
+    }
+
+    @Override
+    public void loadComplete(int nbPages) {
+//        System.out.println("pageNumber" + pageNumber);
+        pdfView.jumpTo(pageNumber, true);
     }
 }
