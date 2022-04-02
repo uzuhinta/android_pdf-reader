894e81c1888751cc3fd5891c6f7f2241d088672a
quannar178 <quannar178@gmail.com>
2022-04-02 11:03:41 +0700

    Done APP without splash

---
+++ app/src/main/java/com/example/pdfreader/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.pdfreader;
+
+import android.view.Window;
+import android.view.WindowManager;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.github.barteksc.pdfviewer.PDFView;
+import com.github.barteksc.pdfviewer.scroll.DefaultScrollHandle;
+import com.github.barteksc.pdfviewer.util.FitPolicy;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE); //will hide the title
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_main);
+
+
+
+        PDFView pdfView = (PDFView) findViewById(R.id.pdfView);
+
+        pdfView.fromAsset("test.pdf")
+//                .pages(0, 2, 1, 3, 3, 3) // all pages are displayed by default
+                .enableSwipe(true) // allows to block changing pages using swipe
+                .swipeHorizontal(false)
+                .enableDoubletap(true)
+                .defaultPage(0)
+                // allows to draw something on the current page, usually visible in the middle of the screen
+//                .onDraw(onDrawListener)
+                // allows to draw something on all pages, separately for every page. Called only for visible pages
+//                .onDrawAll(onDrawListener)
+//                .onLoad(onLoadCompleteListener) // called after document is loaded and starts to be rendered
+//                .onPageChange(onPageChangeListener)
+//                .onPageScroll(onPageScrollListener)
+//                .onError(onErrorListener)
+//                .onPageError(onPageErrorListener)
+//                .onRender(onRenderListener) // called after document is rendered for the first time
+                // called on single tap, return true if handled, false to toggle scroll handle visibility
+//                .onTap(onTapListener)
+//                .onLongPress(onLongPressListener)
+                .enableAnnotationRendering(false) // render annotations (such as comments, colors or forms)
+                .password(null)
+                .scrollHandle(null)
+                .enableAntialiasing(true) // improve rendering a little bit on low-res screens
+                // spacing between pages in dp. To define spacing color, set view background
+                .spacing(0)
+                .autoSpacing(false) // add dynamic spacing to fit each page on its own on the screen
+//                .linkHandler(DefaultLinkHandler)
+                .pageFitPolicy(FitPolicy.WIDTH) // mode to fit pages in the view
+                .fitEachPage(false) // fit each page to the view, else smaller pages are scaled relative to largest page.
+                .pageSnap(false) // snap pages to screen boundaries
+                .pageFling(false) // make a fling change only a single page like ViewPager
+                .nightMode(false) // toggle night mode
+                .load();
+
+    }
+}
